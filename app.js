09205f1fef9d2a2f82dc2982cd538423d8207c21
Maarma <Maarma25@gmail.com>
2024-02-17 11:13:12 +0200

    +- buttons added

--- app.js
+++ app.js
@@ -6,3 +6,29 @@ const boardSize = 7;
 const number = new Number();
 const gameBoard = new GameBoard(boardSize);
 gameBoard.draw(number.getCoordinates());
+
+const homeMinus1 = document.getElementById("homeMinus1");
+const homePlus1 = document.getElementById("homePlus1");
+const homeScore = document.getElementById("homeScore");
+
+var score = 0;
+homeScore.innerHTML = score;
+
+const handleHomeScorePlus = () => {
+    score++;
+    if (score > 99){
+        score = 0;
+    }
+    homeScore.innerHTML = score;
+  };
+
+  const handleHomeScoreMinus = () => {
+    score--;
+    if (score < 0){
+        score = 99;
+    }
+    homeScore.innerHTML = score;
+  };
+
+  homePlus1.addEventListener("click", handleHomeScorePlus);
+  homeMinus1.addEventListener("click", handleHomeScoreMinus);
