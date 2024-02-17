20e144cb1053c8d65f835ed13a0a9c3e911f52b1
Maarma <Maarma25@gmail.com>
2024-02-17 13:47:56 +0200

    test added

--- src/Number.js
+++ src/Number.js
@@ -5,16 +5,18 @@ class Number {
     getCoordinates () {
 
         const coordinates = []
-        let zero = [0,1,2,3,4,5];
+        let zero = [0,1,2,3,4,5,6];
 
-        for (let i = 0; i < 6; i++){
+        for (let i = 0; i < 7; i++){
             let x = zero[i];
-            console.log(x);
-            console.log(i + '-' + x);
-            console.log(coordinates)
+            //console.log(x);
+            //console.log(i + '-' + x);
+            //console.log(coordinates)
             coordinates.push(i + '-' + x);
+            
         }
-        console.log(coordinates)
+        return coordinates;
+        console.log(this.coordinates)
         return this.coordinates;
     }
 }
